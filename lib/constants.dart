e9533d2fab96e302b105866ad2a90e11d8ed9011
Javier E. <jaedns@gmail.com>
2026-05-27 14:46:06 -0300

    Add board item footer and visibility icon (#28)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
+const colorBoardItemBackground = Color(0xFF484041);
 const colorChip = Color(0xFFDBFCFF);
-const colorHeartFilled = Color(0xFFE56399);
-const colorWatchedFilled = Color(0xFFc3d350);
-const colorCounter = Color(0xFFF3EAF4);
-const colorGenreChip = Color(0xFFC3D350);
-const colorPlayIcon = Color(0x999E9E9E);
 const colorPrimary = Color(0xFF1997a2);
 const colorTranslucent = Color(0x22000000);
 
