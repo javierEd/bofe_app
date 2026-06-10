e19d3eb54941d4f36de9873ba6f0f30e55f4b5ca
Javier E. <jaedns@gmail.com>
2026-06-10 10:39:57 -0300

    fix imports

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -5,7 +5,6 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import '../build_context.dart';
 import '../components.dart';
 import '../components/form_container.dart';
-import '../components/query_result_builder.dart';
 import '../components/screen_title.dart';
 import '../components/snackbar_alert.dart';
 import '../graphql/mutations/update_profile.graphql.dart';
